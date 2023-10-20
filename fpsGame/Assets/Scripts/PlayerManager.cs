f89484bede4afef677838a2c535d48086ac1e7b8
Akshit Sharma <akshitsharma196@gmail.com>
2023-10-21 00:10:28 +0530

    Second Commit

---
+++ fpsGame/Assets/Scripts/PlayerManager.cs
@@ -0,0 +1,25 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerManager : MonoBehaviour
+{
+
+
+    public float Speed = 10f;
+    
+
+    public GameObject orientation = null;
+    public GameObject cameraNode = null;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
